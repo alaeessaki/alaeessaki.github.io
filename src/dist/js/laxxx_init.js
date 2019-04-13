b947a7ce55c64374c5cc70f45c80cc03c2c9fc51
Alae Es-saki <alaeessaki@gmail.com>
2019-04-13 17:06:06 +0100

    laxx prb

--- src/dist/js/laxxx_init.js
+++ src/dist/js/laxxx_init.js
@@ -1,10 +1,10 @@
-window.onload = function () {
-	lax.setup() // init
+// window.onload = function () {
+// 	lax.setup() // init
 
-	const updateLax = () => {
-		lax.update(window.scrollY)
-		window.requestAnimationFrame(updateLax)
-	}
+// 	const updateLax = () => {
+// 		lax.update(window.scrollY)
+// 		window.requestAnimationFrame(updateLax)
+// 	}
 
-	window.requestAnimationFrame(updateLax)
-}
+// 	window.requestAnimationFrame(updateLax)
+// }
